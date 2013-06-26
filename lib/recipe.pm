04bcdafbc1d6cfd2505bd97406b06a61d61879a2
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-26 04:03:20 +0000

    contact-us form

--- lib/recipe.pm
+++ lib/recipe.pm
@@ -1,10 +1,35 @@
 package recipe;
 use Dancer ':syntax';
+use JSON -convert_blessed_universally;
+use File::Slurp qw(read_file  write_file);
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'home';
+};
+
+get '/contact-us' => sub {
+    template 'contact_us', {errors => "Please note! email and message fields are required"};
+};
+
+post '/contact-us' => sub {
+    my $file = config->{recipe}{contact_messages};
+    my $json = -e $file ? read_file $file : '{}';
+    my $data = from_json $json;
+    my $now = time;
+    $data->{$now} = {
+        name => params->{user},
+        email => params->{email},
+        message => params->{message},
+    };
+    write_file $file, to_json $data;
+
+    redirect '/contact-us-thanks';
+};
+
+get '/contact-us-thanks' => sub {
+    template 'contact_us_thanks';
 };
 
 true;
