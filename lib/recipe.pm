bac26dda2f6066c9f71b62ea2fe84ababd25eed1
Akshatha Raj <akshatharaj31@gmail.com>
2013-06-26 16:17:05 +0000

    add reipe styling and placeholder recipe listing

--- lib/recipe.pm
+++ lib/recipe.pm
@@ -1,6 +1,6 @@
 package recipe;
 use Dancer ':syntax';
-use JSON -convert_blessed_universally;
+use JSON::PP;
 use File::Slurp qw(read_file  write_file);
 
 our $VERSION = '0.1';
@@ -10,20 +10,23 @@ get '/' => sub {
 };
 
 get '/contact-us' => sub {
-    template 'contact_us', {errors => "Please note! email and message fields are required"};
+    template 'contact_us', {errors => "Please note! email and message fields are required", 
+                            tip_text => "Please use this form to contact us if you 
+                                         have any problems using the site or if you 
+                                         wanted to report any content on the site"};
 };
 
 post '/contact-us' => sub {
     my $file = config->{recipe}{contact_messages};
     my $json = -e $file ? read_file $file : '{}';
-    my $data = from_json $json;
+    my $data = decode_json $json;
     my $now = time;
     $data->{$now} = {
         name => params->{user},
         email => params->{email},
         message => params->{message},
     };
-    write_file $file, to_json $data;
+    write_file $file, encode_json $data;
 
     redirect '/contact-us-thanks';
 };
@@ -32,4 +35,36 @@ get '/contact-us-thanks' => sub {
     template 'contact_us_thanks';
 };
 
+get '/post-recipe' => sub {
+    template 'post-recipe';
+};
+
+post '/post-recipe' => sub {
+    if (! params->{name} eq '' && ! params->{ingredients} eq '' && ! params->{procedure} eq '') {
+        my $file = config->{recipe}{recipes};
+        my $json = -e $file ? read_file $file : '{}';
+        my $data = decode_json $json;
+        my $now = time;
+        $data->{$now} = {
+            name => params->{name},
+            type => params->{type},
+            preparation_time => params->{prep_time},
+            ingredients => params->{ingredients},
+            procedure => params->{procedure},
+        };
+        write_file $file, encode_json $data;
+
+        redirect '/recipe-listing'; 
+    }   
+    else {
+        template 'post-recipe', {'errors' => 'Please note, Name, Ingredients and Procedure are required fields'};
+    }
+};
+
+get '/recipe-listing' => sub {
+    my $file = config->{recipe}{recipes};
+    my $json = -e $file ? read_file $file : '{}';
+    my $data = decode_json $json;
+    template 'recipe-listing', {'recipes' => $data};
+};
 true;
